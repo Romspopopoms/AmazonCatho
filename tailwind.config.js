99ace753a2b7f4d087211bd5b817452864800e7b
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-06-04 16:56:33 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./api/**/*.{js,jsx,ts,tsx}",
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
