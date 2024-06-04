868f21ff799a0f02e273509265a187767b733816
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-06-04 16:47:54 +0200

    Update addSousSection.js

--- api/addSousSection.js
+++ api/addSousSection.js
@@ -7,7 +7,7 @@ const pool = new Pool({
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
-        return res.status(405).json({ message: 'Method not allowed' });
+        return res.status(408).json({ message: 'Method not allowed' });
     }
     
     const { section, subsection} = req.body;
