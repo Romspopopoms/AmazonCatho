902e49ad86fb30783930c89ec46f71e878996bb7
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-06-04 16:35:08 +0200

    Update addSousSection.js

--- api/addSousSection.js
+++ api/addSousSection.js
@@ -1,4 +1,3 @@
-/*import { put } from '@vercel/blob';
 import { Pool } from 'pg';
 
 const pool = new Pool({
@@ -11,11 +10,11 @@ export default async function handler(req, res) {
         return res.status(405).json({ message: 'Method not allowed' });
     }
     
-    const { title, section, description, videoUrl, link, imageUrl } = req.body;
+    const { section, subsection} = req.body;
     try {
         const embedUrl = convertToEmbedURL(videoUrl);
-        const query = 'INSERT INTO articles (title, section, description, imageUrl, videoUrl, link) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
-        const params = [title, section, description, imageUrl, embedUrl, link];
+        const query = 'INSERT INTO section (section, subsection) VALUES ($1, $2) RETURNING *';
+        const params = [section, subsection];
         const { rows } = await pool.query(query, params);
         return res.status(200).json(rows[0]);
     } catch (error) {
@@ -23,9 +22,3 @@ export default async function handler(req, res) {
         return res.status(500).json({ message: "Internal server error" });
     } 
 }
-
-function convertToEmbedURL(url) {
-    const youtubeRegex = /(?:https?:\/\/)?(?:www\.)?(youtube\.com\/watch\?v=|youtu.be\/)([a-zA-Z0-9_-]{11})/;
-    const match = url.match(youtubeRegex);
-    return match ? `https://www.youtube.com/embed/${match[2]}` : url;
-}*/
