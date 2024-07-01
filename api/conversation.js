423cb7806e30d4ff678694243477889373803345
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-07-01 12:35:30 +0200

    Votre message de commit

--- api/conversation.js
+++ api/conversation.js
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 import dotenv from 'dotenv';
-import { handleUserInput } from '../../utils/conversationManager';
+import { handleUserInput } from '../utils/conversationManager';
 
 dotenv.config();
 
